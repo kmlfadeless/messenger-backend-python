1615e60b0bbfd2a49202d38182498cc41b1eba01
Kostiantyn Lozhkin <kostiantyn.lozhkin@valmano.de>
2019-01-30 11:51:52 +0100

    Registration controller

--- v1/db.py
+++ v1/db.py
@@ -2,10 +2,9 @@ import pymongo
 
 
 def db():
-    client = pymongo.MongoClient('mongodb://kml-shard-00-00-ijrny.mongodb.net:27017,kml-shard-00-01-ijrny.mongodb.net:27017,kml-shard-00-02-ijrny.mongodb.net:27017/test?ssl=true&replicaSet=KML-shard-0&authSource=admin&retryWrites=true')
     f = open("passwords.txt", "r")
     lines = f.readlines()
-    username = lines[0]
-    password = lines[1]
+    credentials = lines[0]
     f.close()
-    return client.the_database.authenticate(username, password, source='testdb')
+    client = pymongo.MongoClient('mongodb+srv://'+credentials+'@kml-ijrny.mongodb.net/test?retryWrites=true')
+    return client.testdb
